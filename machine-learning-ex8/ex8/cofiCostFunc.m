4a5f9d503d2c986e62334910b3c1a4bbaf10a3b1
vorpus <li3zhang@gmail.com>
2018-09-16 21:51:03 -0400

    finish

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -11,7 +11,7 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
+
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -21,7 +21,7 @@ Theta_grad = zeros(size(Theta));
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
+%               matches our costs. After that, you should implement the
 %               gradient and use the checkCostFunction routine to check
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
@@ -29,25 +29,42 @@ Theta_grad = zeros(size(Theta));
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the
 %            i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
+%        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        Theta_grad - num_users x num_features matrix, containing the
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+inner = (((X * Theta') - Y) .^ 2) .* R;
+J = sum(sum(inner)) / 2;
+hlam = lambda / 2;
+J = J + hlam * sum(sum(Theta .^ 2)) + hlam * sum(sum(X .^ 2));
 
+for i = 1:num_movies
+  idx = find(R(i,:) == 1);
+  Thetatemp = Theta(idx,:);
+  Ytemp = Y(i, idx);
+  Xi = X(i,:);
+  X_grad(i, :) = ((Xi * Thetatemp') - Ytemp) * Thetatemp;
+  X_grad(i, :) = X_grad(i, :) + lambda * Xi;
+%  if lambda != 0
+%    display(X_grad(i,:));
+%    display(lambda * Xi);
+%  end
+end
 
-
-
-
-
-
-
+for j = 1:num_users
+  idx = find(R(:,j) == 1);
+  Xtemp = X(idx,:);
+  Ytemp = Y(idx, j);
+  Thetaj = Theta(j,:);
+  Theta_grad(j,:) = ((Xtemp * Thetaj') - Ytemp)' * X(idx,:) + lambda * Thetaj;
+end
 
 
 
